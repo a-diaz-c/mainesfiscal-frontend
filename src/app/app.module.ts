37a02d50a688cd2018d35fea60ce212d0ca604c5
a-diaz-c <diaz1517@hotmail.com>
2020-02-26 13:19:51 -0600

    agrego modulo shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { NofoundpageComponent } from './404/nofoundpage/nofoundpage.component';
 import { PagesModule } from './pages/pages.module';
 
 import { APP_ROUTES } from "./app.routes";
+import { SliderComponent } from './shared/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NofoundpageComponent,
+    SliderComponent,
   ],
   imports: [
     BrowserModule,
