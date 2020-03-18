f920a29e4f04dd99c98792a3244082be9378ce18
jose <jseh125@gmail.com>
2020-03-18 10:21:39 -0600

    se agregaron  las vistas de configuracion

---
+++ src/app/pages/alta-empresa/alta-empresa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alta-empresa',
+  templateUrl: './alta-empresa.component.html',
+  styleUrls: ['./alta-empresa.component.css']
+})
+export class AltaEmpresaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
